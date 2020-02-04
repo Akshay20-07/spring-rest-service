7cdbca1d3451475ceb667e1d67a511a1bd10b8f6
ezaksch <akshaychauhanjp@gmail.com>
2020-02-04 16:27:13 +0530

    implemented the response for created DAo

--- src/main/java/com/example/restwebservices/bean/user/UserResource.java
+++ src/main/java/com/example/restwebservices/bean/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.example.restwebservices.bean.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * @author ezaksch
@@ -36,8 +39,10 @@ public class UserResource {
 	}
 	
 	@PostMapping(path="/users")
-	public void createUser(@RequestBody User user){
-		service.save(user);
+	public ResponseEntity<Object> createUser(@RequestBody User user){
+		User saved=service.save(user);
+		URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saved.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
